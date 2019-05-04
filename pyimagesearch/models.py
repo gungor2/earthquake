92999f7d85d13e38ca9bbd9ef1cf27ca7aff0801
gungor2 <gungor2@illinois.edu>
2019-05-04 13:14:05 -0500

    sub_directoroies

--- pyimagesearch/models.py
+++ pyimagesearch/models.py
@@ -40,14 +40,15 @@ def create_cnn(width, height, depth, filters=(16, 32, 64), regress=False):
 			x = inputs
 
 		# CONV => RELU => BN => POOL
-		x = Conv2D(f, (3, 3), padding="same")(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+		x = Conv2D(f, (5, 5), padding="same")(x)
 		x = Activation("relu")(x)
 		x = BatchNormalization(axis=chanDim)(x)
 		x = MaxPooling2D(pool_size=(2, 2))(x)
 
 	# flatten the volume, then FC => RELU => BN => DROPOUT
 	x = Flatten()(x)
-	x = Dense(16)(x)
+	x = Dense(10)(x)
 	x = Activation("relu")(x)
 	x = BatchNormalization(axis=chanDim)(x)
 	x = Dropout(0.5)(x)
